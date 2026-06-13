bd9e27a866d0fa34e6c8c6240218e50b0770e2bd
acrisrosa <cris2022pereira@gmail.com>
2026-06-13 20:13:06 +0100

    main.html e header.html

--- exerc_react_Ana/src/app/components/header/header.ts
+++ exerc_react_Ana/src/app/components/header/header.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { Avatar } from "../avatar/avatar";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, Avatar],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
